f19052ccd3c0cbc12ef8cafd119ce4a9486ddbd6
nikhil <NIKHIL.GUPTA@PEARSON.COM>
2019-02-03 22:24:25 +0530

    added specialities map impl.

---
+++ spring-data/src/main/java/com/nikhilgupta/springframework/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.nikhilgupta.springframework.services;
+
+import com.nikhilgupta.springframework.models.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
